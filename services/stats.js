294dfa44e0d74344285540f421f51a5f54e07eab
Elioy <eliot.barus@gmail.com>
2020-02-17 16:06:19 +0100

    Trouver le top 5 des types et sous types d'anomalies: done

--- services/stats.js
+++ services/stats.js
@@ -24,12 +24,52 @@ exports.statsByArrondissement = (client, callback) => {
                     count: data.doc_count
                 };
             }));
+        }).catch((err) => {
+            console.log(err);
         });
 }
 
 exports.statsByType = (client, callback) => {
-    // TODO Trouver le top 5 des types et sous types d'anomalies
-    callback([]);
+    client
+        .search({
+            index: indexName,
+            body: {
+                size: 0,
+                aggs: {
+                    type: {
+                        terms: {
+                            field: "type.keyword",
+                            size: 5
+                        },
+                        aggs: {
+                            sous_type: {
+                                terms: {
+                                    field: "sous_type.keyword",
+                                    size: 5
+                                }
+                            }
+                        }
+                    }
+
+                }
+            }
+        })
+        .then(resp => {
+            callback(resp.body.aggregations.type.buckets.map((data) => {
+                return {
+                    type: data.key,
+                    count: data.doc_count,
+                    sous_types: data.sous_type.buckets.map((sous_type) => {
+                        return {
+                            sous_type: sous_type.key,
+                            count: sous_type.doc_count,
+                        }
+                    })
+                };
+            }));
+        }).catch((err) => {
+            console.log(err);
+        });
 }
 
 exports.statsByMonth = (client, callback) => {
